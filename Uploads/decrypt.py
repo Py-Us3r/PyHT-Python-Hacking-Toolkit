955cc8c35594c81ec40dcc8ea3ce9dd1705a30f6
Py-Us3r <pyus3r@gmail.com>
2025-04-29 13:21:21 +0200

    Update decrypt.py

--- Uploads/decrypt.py
+++ Uploads/decrypt.py
@@ -22,3 +22,23 @@ if sys.argv[1] == file_path:
       archivos = f.read().strip().split(',')
 else:
     archivos = sys.argv[1].strip().split(",")
+
+
+
+for archivo in archivos:
+    if os.path.exists(archivo):  
+        with open(archivo, "rb") as file:
+            datos_encriptados = file.read() 
+
+        try:
+            datos = fernet.decrypt(datos_encriptados)
+            with open(archivo, "wb") as file:
+                file.write(datos)
+
+
+        except Exception as e:
+            pass
+    else:
+        print(f"File ‘{archivo}’ does not exist.")
+
+print(f"Files has been decrypted successfully.")
